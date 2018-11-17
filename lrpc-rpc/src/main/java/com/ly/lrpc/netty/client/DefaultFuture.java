eb8de189c99eb346de78a7fdd0d612e21d281d60
[yang.liu] <[yang.liu@husor.com123]>
2018-11-17 11:32:56 +0800

    build

--- lrpc-rpc/src/main/java/com/ly/lrpc/netty/client/DefaultFuture.java
+++ lrpc-rpc/src/main/java/com/ly/lrpc/netty/client/DefaultFuture.java
@@ -1,6 +1,8 @@
 package com.ly.lrpc.netty.client;
 
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -11,10 +13,27 @@ public class DefaultFuture {
     final Lock lock = new ReentrantLock();
     private Condition condition = lock.newCondition();
     private Response response;
+    private long timeout = 2*60*1000;
+    private long startTime = System.currentTimeMillis();
     public DefaultFuture(ClientRequest request) {
         allDefaultFuture.put(request.getId(),this);
     }
 
+    public long getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(long timeout) {
+        this.timeout = timeout;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
 
     private boolean done(){
         if(this.getResponse()!=null){
@@ -55,6 +74,24 @@ public class DefaultFuture {
         return this.response;
     }
 
+    public Response get(long time){
+        lock.lock();
+        try {
+            while (!done()){
+                condition.await(time,TimeUnit.SECONDS);
+                if(System.currentTimeMillis() - startTime > time){
+                    System.out.println("请求超时");
+                    break;
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            lock.unlock();
+        }
+        return this.response;
+    }
+
     public Response getResponse() {
         return response;
     }
@@ -62,4 +99,36 @@ public class DefaultFuture {
     public void setResponse(Response response) {
         this.response = response;
     }
+
+    static class FutureThread extends Thread{
+
+        @Override
+        public void run() {
+           Set<Long> ids = allDefaultFuture.keySet();
+
+           for (Long id:ids){
+               DefaultFuture df = allDefaultFuture.get(id);
+               if(df == null){
+                   allDefaultFuture.remove(df);
+               }else {
+                   if(df.getTimeout()<(System.currentTimeMillis() - df.getStartTime())){
+                       Response resp = new Response();
+                       resp.setId(id);
+                       resp.setCode("333333");
+                       resp.setMessage("链路请求超时");
+                       recive(resp);
+                   }
+               }
+           }
+        }
+    }
+
+
+    static {
+        FutureThread futureThread = new FutureThread();
+        futureThread.setDaemon(true);
+        futureThread.run();
+    }
+
+
 }
